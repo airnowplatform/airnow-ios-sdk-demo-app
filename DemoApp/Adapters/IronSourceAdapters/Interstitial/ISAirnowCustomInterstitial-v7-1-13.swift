99e073b4ad01ca5be07dd237057ee1c94f74c3b8
Gusakovsky, Sergey <sgusakovsky@qulix.com>
2022-02-15 13:29:30 +0300

    Update IronSource mediation adapters (v 1.0.2)

--- DemoApp/Adapters/IronSourceAdapters/Interstitial/ISAirnowCustomInterstitial-v7-1-13.swift
+++ DemoApp/Adapters/IronSourceAdapters/Interstitial/ISAirnowCustomInterstitial-v7-1-13.swift
@@ -1,21 +1,22 @@
 //
-//  ISAirnowCustomBanner.swift
+//  ISAirnowCustomInterstitial.swift
 //  AirnowMediationWithIS
 //
-//  Created by Gusakovsky, Sergey on 15.12.21.
+//  Created by Gusakovsky, Sergey on 14.12.21.
 //
 
 import Foundation
 import ObjectiveC.runtime
 import AirMonetizationSDK
 
-@objc(ISAirnowCustomBanner)
-public class ISAirnowCustomBanner: ISBaseAdAdapter, AirMonetizationAdDelegate {
-        
-    @objc private var banner: AirMotetizationAd?
-    
+//MARK: - Use this interstitial adapter only for IronSource SDK version less then 7.2.0
+@objc(ISAirnowCustomInterstitial)
+public class ISAirnowCustomInterstitial: ISBaseAdAdapter, AirMonetizationAdDelegate {
+
+    @objc private var interstitialAd: AirMotetizationAd?
+
     @objc private weak var adapterDelegate: ISAdapterAdDelegate? = nil
-    
+
     @objc public override func loadAd(with adData: ISAdData, delegate: ISAdapterAdDelegate) {
         guard
             let apiKey = adData.configuration[ISAdapterConstants.ExtraKey.APIKey] as? String,
@@ -29,94 +30,94 @@ public class ISAirnowCustomBanner: ISBaseAdAdapter, AirMonetizationAdDelegate {
             )
             return
         }
-        
+
         //MARK: - TODO
         AirMonetization.shared.setAPIKey(key: apiKey, appID: appId)
         AirMonetization.shared.setTestMode(testMode: false)
-        
+
         self.adapterDelegate = delegate
-        
+
         DispatchQueue.main.async {
-            if self.banner != nil {
-                self.banner?.removeFromViewController()
-                self.banner?.stop()
-                self.banner?.dismiss()
-                self.banner = nil
+            if self.interstitialAd != nil {
+                self.interstitialAd?.removeFromViewController()
+                self.interstitialAd?.stop()
+                self.interstitialAd?.dismiss()
+                self.interstitialAd = nil
             }
-            
-            self.banner = AirMonetizationInappBanner.inappBanner()
+
+            self.interstitialAd = AirMonetizationInterstitial.interstitial()
             if let placementName = adData.configuration[ISAdapterConstants.ExtraKey.placementName] as? String {
-                self.banner?.setPlacementName(placementName: placementName)
+                self.interstitialAd?.setPlacementName(placementName: placementName)
             }
-            
-            self.banner?.setSource(source: "IronSource")
-            self.banner?.delegate = self
-            self.banner?.load()
+
+            self.interstitialAd?.setSource(source: "IronSource")
+            self.interstitialAd?.delegate = self
+            self.interstitialAd?.load()
         }
     }
-    
+
     @objc public override func isAdAvailable(with adData: ISAdData) -> Bool {
-        return banner?.isCanOpen ?? false
+        return interstitialAd?.isCanOpen ?? false
     }
-    
+
     @objc public override func showAd(with viewController: UIViewController, adData: ISAdData, delegate: ISAdapterAdDelegate) {
-        
+        if let interstitialAd = interstitialAd, interstitialAd.isCanOpen {
+            interstitialAd.show()
+        }
     }
 
     //MARK: - AirMonetizationAdDelegate Protocol
     public func adDidStartLoad(ad: AirMotetizationAd) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
     }
-    
+
     public func adDidFinishLoad(ad: AirMotetizationAd) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
         adapterDelegate?.adDidLoad()
-        if let banner = banner, banner.isCanOpen {
-            banner.show()
-        }
     }
-    
+
     public func adDidShow(ad: AirMotetizationAd) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
         adapterDelegate?.adDidOpen()
     }
-    
+
     public func adDidClick(ad: AirMotetizationAd) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
         adapterDelegate?.adDidClick()
         adapterDelegate?.adDidClose()
     }
-    
+
     public func adDidClose(ad: AirMotetizationAd) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
         adapterDelegate?.adDidClose()
     }
-    
+
     public func adDidComplete(ad: AirMotetizationAd) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
     }
-    
+
     public func adDidDismiss(ad: AirMotetizationAd) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
+        adapterDelegate?.adDidClose()
     }
-    
+
     public func adDidFailWithError(ad: AirMotetizationAd, error: String) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function). Error: \(error)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function). Error: \(error)")
         #endif
         adapterDelegate?.adDidFailToLoadWith(
             .internal,
@@ -124,15 +125,15 @@ public class ISAirnowCustomBanner: ISBaseAdAdapter, AirMonetizationAdDelegate {
             errorMessage: ISAirMonetizationAdapterError.interstitialNotAvailable.description
         )
     }
-    
+
     public func adDidShowFailWithError(ad: AirMotetizationAd, error: String) {
         #if DEBUG
-        print("[AirMonetization IronSource Adapter Banner] AirMonetizationAdDelegate: \(#function). Error: \(error)")
+        print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function). Error: \(error)")
         #endif
         adapterDelegate?.adDidFailToShowWithErrorCode(
             Int32(ISAdapterErrors.missingParams.rawValue),
             errorMessage: ISAirMonetizationAdapterError.interstitialNotAvailable.description
         )
     }
-    
+
 }
--- DemoApp/Adapters/IronSourceAdapters/Interstitial/ISAirnowCustomInterstitial-v7-2-0.swift
+++ DemoApp/Adapters/IronSourceAdapters/Interstitial/ISAirnowCustomInterstitial-v7-2-0.swift
@@ -1,22 +1,23 @@
 //
-//  ISAirnowCustomInterstitial.swift
+//  ISAirnowCustomInterstitial-v7-2-0.swift
 //  AirnowMediationWithIS
 //
-//  Created by Gusakovsky, Sergey on 14.12.21.
+//  Created by Gusakovsky, Sergey on 15.02.22.
 //
 
 import Foundation
 import ObjectiveC.runtime
 import AirMonetizationSDK
 
+//MARK: - Use this interstitial adapter only for IronSource SDK 7.2.0+
 @objc(ISAirnowCustomInterstitial)
-public class ISAirnowCustomInterstitial: ISBaseAdAdapter, AirMonetizationAdDelegate {
-        
+public class ISAirnowCustomInterstitial: ISBaseInterstitial, AirMonetizationAdDelegate {
+
     @objc private var interstitialAd: AirMotetizationAd?
-    
-    @objc private weak var adapterDelegate: ISAdapterAdDelegate? = nil
-    
-    @objc public override func loadAd(with adData: ISAdData, delegate: ISAdapterAdDelegate) {
+
+    @objc private weak var adapterDelegate: ISInterstitialAdDelegate? = nil
+
+    @objc public override func loadAd(with adData: ISAdData, delegate: ISInterstitialAdDelegate) {
         guard
             let apiKey = adData.configuration[ISAdapterConstants.ExtraKey.APIKey] as? String,
             let appId = adData.configuration [ISAdapterConstants.ExtraKey.appID] as? String
@@ -24,18 +25,18 @@ public class ISAirnowCustomInterstitial: ISBaseAdAdapter, AirMonetizationAdDeleg
         {
             delegate.adDidFailToLoadWith(
                 .internal,
-                errorCode: Int32(ISAdapterErrors.missingParams.rawValue),
+                errorCode: ISAdapterErrors.missingParams.rawValue,
                 errorMessage: ISAirMonetizationAdapterError.noApiKeyOrAppId.description
             )
             return
         }
-        
+
         //MARK: - TODO
         AirMonetization.shared.setAPIKey(key: apiKey, appID: appId)
         AirMonetization.shared.setTestMode(testMode: false)
-        
+
         self.adapterDelegate = delegate
-        
+
         DispatchQueue.main.async {
             if self.interstitialAd != nil {
                 self.interstitialAd?.removeFromViewController()
@@ -43,23 +44,23 @@ public class ISAirnowCustomInterstitial: ISBaseAdAdapter, AirMonetizationAdDeleg
                 self.interstitialAd?.dismiss()
                 self.interstitialAd = nil
             }
-            
+
             self.interstitialAd = AirMonetizationInterstitial.interstitial()
             if let placementName = adData.configuration[ISAdapterConstants.ExtraKey.placementName] as? String {
                 self.interstitialAd?.setPlacementName(placementName: placementName)
             }
-            
+
             self.interstitialAd?.setSource(source: "IronSource")
             self.interstitialAd?.delegate = self
             self.interstitialAd?.load()
         }
     }
-    
+
     @objc public override func isAdAvailable(with adData: ISAdData) -> Bool {
         return interstitialAd?.isCanOpen ?? false
     }
-    
-    @objc public override func showAd(with viewController: UIViewController, adData: ISAdData, delegate: ISAdapterAdDelegate) {
+
+    @objc public override func showAd(with viewController: UIViewController, adData: ISAdData, delegate: ISInterstitialAdDelegate) {
         if let interstitialAd = interstitialAd, interstitialAd.isCanOpen {
             interstitialAd.show()
         }
@@ -71,21 +72,21 @@ public class ISAirnowCustomInterstitial: ISBaseAdAdapter, AirMonetizationAdDeleg
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
     }
-    
+
     public func adDidFinishLoad(ad: AirMotetizationAd) {
         #if DEBUG
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
         adapterDelegate?.adDidLoad()
     }
-    
+
     public func adDidShow(ad: AirMotetizationAd) {
         #if DEBUG
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
         adapterDelegate?.adDidOpen()
     }
-    
+
     public func adDidClick(ad: AirMotetizationAd) {
         #if DEBUG
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
@@ -93,45 +94,46 @@ public class ISAirnowCustomInterstitial: ISBaseAdAdapter, AirMonetizationAdDeleg
         adapterDelegate?.adDidClick()
         adapterDelegate?.adDidClose()
     }
-    
+
     public func adDidClose(ad: AirMotetizationAd) {
         #if DEBUG
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
         adapterDelegate?.adDidClose()
     }
-    
+
     public func adDidComplete(ad: AirMotetizationAd) {
         #if DEBUG
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
     }
-    
+
     public func adDidDismiss(ad: AirMotetizationAd) {
         #if DEBUG
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function)")
         #endif
+        adapterDelegate?.adDidClose()
     }
-    
+
     public func adDidFailWithError(ad: AirMotetizationAd, error: String) {
         #if DEBUG
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function). Error: \(error)")
         #endif
         adapterDelegate?.adDidFailToLoadWith(
             .internal,
-            errorCode: Int32(ISAdapterErrors.internal.rawValue),
+            errorCode: ISAdapterErrors.internal.rawValue,
             errorMessage: ISAirMonetizationAdapterError.interstitialNotAvailable.description
         )
     }
-    
+
     public func adDidShowFailWithError(ad: AirMotetizationAd, error: String) {
         #if DEBUG
         print("[AirMonetization IronSource Adapter Interstitial] AirMonetizationAdDelegate: \(#function). Error: \(error)")
         #endif
         adapterDelegate?.adDidFailToShowWithErrorCode(
-            Int32(ISAdapterErrors.missingParams.rawValue),
+            ISAdapterErrors.missingParams.rawValue,
             errorMessage: ISAirMonetizationAdapterError.interstitialNotAvailable.description
         )
     }
-    
+
 }
